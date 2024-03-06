ca3ceb8690eb31f31037809ca24711b6e8158a83
AH-Designing <anushussain298@gmail.com>
2024-03-06 16:22:02 +0500

    third commit

---
+++ assignment #11/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
+let favTransportation = ["helicopter", "bike", "car", "jeep", "airplane"];
+// Print statements
+for (let i = 0; i < favTransportation.length; i++) {
+    console.log(`I would like to own a ${favTransportation[i]}.`);
+}
