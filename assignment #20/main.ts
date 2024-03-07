b0730c84494317ab126de470c92e1f15d2cf67cd
AH-Designing <anushussain298@gmail.com>
2024-03-08 03:39:33 +0500

    add files

---
+++ assignment #20/main.ts
@@ -0,0 +1,6 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+//array:
+let fruits: string[] = ["Apple", "Mango", "Banana", "Grapes", "Orange"];
+
+// Print the array 
+console.log("List of fruits:", fruits);
