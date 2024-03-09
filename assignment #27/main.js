111aca8b7eb01b7332076623f324702ecad96a12
AH-Designing <anushussain298@gmail.com>
2024-03-09 16:46:02 +0500

    add files

---
+++ assignment #27/main.js
@@ -0,0 +1,35 @@
+"use strict";
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+//first version of the program
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color === 'yellow') {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("You just earned 15 points for shooting the red alien.");
+}
+//second version of the program
+let alienColor = 'yellow';
+if (alienColor === 'green') {
+    console.log("You just earned 5 points for shooting the green alien.");
+}
+else if (alienColor === 'yellow') {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+}
+else if (alienColor === 'red') {
+    console.log("You just earned 15 points for shooting the red alien.");
+}
+//third version of the program
+let aliencolor = 'red';
+if (aliencolor === 'green') {
+    console.log("You just earned 5 points for shooting the green alien.");
+}
+else if (aliencolor === 'yellow') {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+}
+else if (aliencolor === 'red') {
+    console.log("You just earned 15 points for shooting the red alien.");
+}
