879adc9e9c7b79e6a8d93ffe7f509d362ec86021
AH-Designing <anushussain298@gmail.com>
2024-03-09 16:24:20 +0500

    add files

---
+++ assignment #25/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+//version of this program which pass
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points.");
+}
+//another version of this program which fails
+let alienColor = 'yellow';
+if (alienColor === 'green') {
+    console.log("You just earned 5 points.");
+}
---
+++ assignment #25/main.ts
@@ -0,0 +1,13 @@
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+//version of this program which pass
+let alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log("You just earned 5 points.");
+}
+//another version of this program which fails
+let alienColor: string = 'yellow';
+
+if (alienColor === 'green') {
+    console.log("You just earned 5 points.");
+}
