53a3cbd0847c75bac7e721a5df73c3a422404264
AH-Designing <anushussain298@gmail.com>
2024-03-09 16:35:21 +0500

    add files

---
+++ assignment #26/main.js
@@ -0,0 +1,18 @@
+"use strict";
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+//first version of the program
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("You just earned 10 points for shooting the alien.");
+}
+//another version of the program
+let alienColor = 'red';
+if (alienColor === 'green') {
+    console.log("You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("You just earned 10 points for shooting the alien.");
+}
