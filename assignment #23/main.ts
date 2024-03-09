4e07052597c74f206a794426bd92467dbc3068b7
AH-Designing <anushussain298@gmail.com>
2024-03-09 15:34:40 +0500

    add files

---
+++ assignment #23/main.ts
@@ -0,0 +1,48 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+
+let password = 'password123';
+let number = 7;
+let login = 'admin';
+let color = 'green';
+let city = 'Karachi';
+let car = 'honda';
+
+//Check if password is equal to 'password123'
+console.log("Is password == 'password123'? I predict True.");
+console.log(password == 'password123');
+
+//Check if number is less than 10
+console.log("Is number < 10? I predict True.");
+console.log(number < 10);
+
+//Check if login is equal to 'admin'
+console.log("Is login == 'admin'? I predict True.");
+console.log(login == 'admin');
+
+//Check if color is not equal to 'red'
+console.log("Is color != 'red'? I predict True.");
+console.log(color != 'red');
+
+//Check if city is equal to 'Los Angeles'
+console.log("Is city == 'Karachi'? I predict True.");
+console.log(city == 'Karachi');
+
+//Check if car is equal to 'toyota'
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota');
+
+//Check if password is equal to 'qwerty'
+console.log("Is password == 'qwerty'? I predict False.");
+console.log(password == 'qwerty');
+
+//Check if number is greater than 5
+console.log("Is number > 5? I predict False.");
+console.log(number <= 5);
+
+//Check if login is equal to 'user'
+console.log("Is login == 'user'? I predict False.");
+console.log(login == 'user');
+
+//Check if color is equal to 'blue'
+console.log("Is color == 'blue'? I predict False.");
+console.log(color == 'blue');
