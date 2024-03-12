227ec6420397e0adba53b4e4369ff31e4f693276
AH-Designing <anushussain298@gmail.com>
2024-03-12 15:56:51 +0500

    add files

---
+++ assignment #29/main.js
@@ -0,0 +1,17 @@
+//Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+var favorite_fruits = ['pineapple', 'banana', 'mango'];
+if (favorite_fruits.includes('pineapple')) {
+    console.log("You really like pineapples!");
+}
+if (favorite_fruits.includes('strawberry')) {
+    console.log("You really like strawberries!");
+}
+if (favorite_fruits.includes('banana')) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes('mango')) {
+    console.log("You really like mangoes!");
+}
+if (favorite_fruits.includes('orange')) {
+    console.log("You really like oranges!");
+}
