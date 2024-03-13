aa412125e0e399c5ec1c555a2d479613f52e3517
AH-Designing <anushussain298@gmail.com>
2024-03-13 15:08:06 +0500

    add files

---
+++ assignment #33/main.ts
@@ -0,0 +1,19 @@
+//Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let number of numbers) {
+    let ordinal: string;
+
+    if (number === 1) {
+        ordinal = number + "st";
+    } else if (number === 2) {
+        ordinal = number + "nd";
+    } else if (number === 3) {
+        ordinal = number + "rd";
+    } else {
+        ordinal = number + "th";
+    }
+
+    console.log(ordinal);
+}
