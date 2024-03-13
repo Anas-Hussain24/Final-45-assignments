c734692b95b1cb05584df1b1ef2bf28c070d43a6
AH-Designing <anushussain298@gmail.com>
2024-03-13 15:23:06 +0500

    add files

---
+++ assignment #34/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+let pizzas = ['BBQ', 'Supreme', 'Hot Feast'];
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log("There's nothing better than a hot slice of pizza!");
---
+++ assignment #34/main.ts
@@ -0,0 +1,9 @@
+//Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+
+let pizzas: string[] = ['BBQ', 'Supreme', 'Hot Feast'];
+
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("There's nothing better than a hot slice of pizza!");
