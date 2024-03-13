f09db245bab31431fbad75286f91e3aec0fb4967
AH-Designing <anushussain298@gmail.com>
2024-03-13 15:58:59 +0500

    add files

---
+++ assignment #36/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+function make_Shirt(size, message) {
+    console.log(`You ordered a ${size} sized t-shirt with the message: "${message}".`);
+}
+make_Shirt('large', 'Pakistan Zindabad!');
---
+++ assignment #36/main.ts
@@ -0,0 +1,8 @@
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+
+function make_Shirt(size: string, message: string): void{
+    console.log(`You ordered a ${size} sized t-shirt with the message: "${message}".`);
+}
+
+make_Shirt('large', 'Pakistan Zindabad!');
+
